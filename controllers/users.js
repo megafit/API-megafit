403c52e5d742866fc99f40a25779d20f289db4f1
prasetiodc <mprasetiodc@gmail.com>
2020-05-15 11:25:23 +0700

    cek bug

--- controllers/users.js
+++ controllers/users.js
@@ -322,7 +322,8 @@ class users {
   static async checkToken(req, res) {
     try {
       let userLogin = await tblUsers.findOne({ where: { userId: req.user.userId }, include: [{ model: tblStaffs }, { model: tblMembers }] })
-
+console.log(req.user.userId)
+console.log(userLogin.userId)
       res.status(200).json({
         nickname: userLogin.nickname,
         fullname: userLogin.fullname,
